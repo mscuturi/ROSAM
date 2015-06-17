2745c1ab472c7cd19f66a4cf7082b0204267fbd1
Joël Brogniart <brogniar@muspellheim-dhcp-159-84-126-151.univ-lyon2.fr>
2015-06-17 14:18:04 +0200

    Change HTML headers to HTML5.

--- view_tasks.php
+++ view_tasks.php
@@ -18,9 +18,8 @@ $interval = 60 * 60 * 6; // 6 hours
 header ("Last-Modified: " . gmdate ('r', $max));
 header ("Expires: " . gmdate ("r", ($max + $interval)));
 header ("Cache-Control: max-age=$interval");
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="fr">
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 	<title>View Tasks</title>
